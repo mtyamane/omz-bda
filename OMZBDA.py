dbf98967346d96c052c87e61a663befce4b02215
Mark Yamane <57078091+mtyamane@users.noreply.github.com>
2021-03-10 13:13:38 -0500

    refactored some functions and added example output

--- OMZBDA.py
+++ OMZBDA.py
@@ -8,9 +8,8 @@ an algorithm used to identify boundaries of OMZ cores.
 from netCDF4 import Dataset
 import numpy as np
 
-def pullData(filename):
-    fpath = 'data/'
-    ds = Dataset(fpath+filename)
+def pullData(fpath, fname):
+    ds = Dataset(fpath+fname)
     
     # pull data
     lons = ds.variables['lon'][:]
@@ -20,9 +19,42 @@ def pullData(filename):
     
     return lons, lats, deps, dO
 
-def getLatSlice(filename, target_lat):
+def centerAtPacific(lons, ys, zs):
+    # centers lons and zs to the Pacific Ocean and
+    # pulls data for the specified longitudinal range
+    
+    # center global map at Pacific
+    for i in range(len(lons)):
+        for j in range(len(lons[0,:])):
+            if lons[i,j] < 0:
+                lons[i,j] += 360
+    # reorder data (-180 - 180  --> 0 - 360)
+    temp = 0
+    for i in range(len(lons)):
+        for j in range(180):
+            temp = lons[i, j+180]
+            lons[i, j+180] = lons[i,j]
+            lons[i, j] = temp
+            temp = zs[i, j+180]
+            zs[i, j+180] = zs[i,j]
+            zs[i,j] = temp
+    
+    # mask to keep only lons of the Pacific Ocean
+    left = 125
+    right = 260
+    pacific_mask = (lons[0] > left) & (lons[0] < right)
+    new_lon = []
+    new_ys = []
+    new_zs = []
+    for i in range(len(lons)):
+        new_lon.append(lons[i][pacific_mask])
+        new_ys.append(ys[i][pacific_mask])
+        new_zs.append(zs[i][pacific_mask])
+    return new_lon, new_ys, new_zs
+
+def getLatSlice(fpath, fname, target_lat):
     # pull data
-    lons, lats, deps, dO = pullData(filename)
+    lons, lats, deps, dO = pullData(fpath, fname)
 
     # create full grids for each dimension
     tempLat = []
@@ -56,33 +88,7 @@ def getLatSlice(filename, target_lat):
     deps = np.transpose(deps)
     dO_slice = dO[:, i_lat, :]
     
-    # center at Pacific
-    for i in range(len(lonVert)):
-        for j in range(len(lonVert[0,:])):
-            if lonVert[i,j] < 0:
-                lonVert[i,j] += 360
-    # reorder data (-180 - 180  --> 0 - 360)
-    temp = 0
-    for i in range(len(lonVert)):
-        for j in range(180):
-            temp = lonVert[i, j+180]
-            lonVert[i, j+180] = lonVert[i,j]
-            lonVert[i, j] = temp
-            temp = dO_slice[i, j+180]
-            dO_slice[i, j+180] = dO_slice[i,j]
-            dO_slice[i,j] = temp
-    
-    # mask to keep only lons of the Pacific
-    left = 125
-    right = 260
-    pacific_mask = (lonVert[0] > left) & (lonVert[0] < right)
-    new_lon = []
-    new_dep = []
-    new_dO = []
-    for i in range(len(lonVert)):
-        new_lon.append(lonVert[i][pacific_mask])
-        new_dep.append(deps[i][pacific_mask])
-        new_dO.append(dO_slice[i][pacific_mask])
+    new_lon, new_dep, new_dO = centerAtPacific(lonVert, deps, dO_slice)
     return i_lat, actual_lat, new_lon, new_dep, new_dO
 
 def omzbda(lons, deps, dO, threshold):
@@ -112,3 +118,22 @@ def omzbda(lons, deps, dO, threshold):
                     omzb_r = (lons[i][j], deps[i][j])
                 
     return top, bottom, left, right, omzb_t, omzb_b, omzb_l, omzb_r
+
+def RUN_OMZBDA(i_fpath, i_fnames, target_lats, o_fpath, o_fname, status = True):
+    # runs OMZ-BDA algorithm on all files from i_fnames and outputs to o_fname
+    month = 1
+    if status:
+        print('Algorithm start...')
+    with open(o_fpath+o_fname, 'x') as f:
+        f.write('month,lat,top,bottom,west,east\n')
+        for i_fname in i_fnames:
+            if status:
+                print('working on month:', month)
+            for target_lat in target_lats:
+                i_lat, actual_lat, lons, deps, dO = getLatSlice(i_fpath, i_fname, target_lat)
+                top, bottom, left, right, t_pt, b_pt, l_pt, r_pt = omzbda(lons, deps, dO, 20)
+                f.write(','.join([str(month), str(actual_lat), str(top), str(bottom), str(left), str(right)]) + '\n')
+            month += 1
+    f.close()
+    if status:
+        print('... algorithm finished. Output is located at: ' + o_fpath + o_fname)
